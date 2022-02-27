9b6b9f340d82749ee8f19795ed28206cfd338cd7
Wazzapio <myster1on.toks@gmail.com>
2022-02-27 21:34:29 +0300

    lesson_3

---
+++ lesson_3/server.py
@@ -0,0 +1,62 @@
+import json
+import socket
+import sys
+
+from common.utils import get_message, send_message
+from common.variables import ACTION, PRESENCE, TIME, USER, ACCOUNT_NAME, RESPONSE, \
+    RESPONSE_DEFAULT_IP_ADDRESS, ERROR, DEFAULT_PORT, MAX_CONNECTIONS
+
+
+def process_client_message(message):  # обработчик сообщений от клиента;
+    if ACTION in message and message[ACTION] == PRESENCE and TIME in message \
+            and USER in message and message[USER][ACCOUNT_NAME] == 'Guest':
+        return {RESPONSE: 200}
+    return {
+        RESPONSE_DEFAULT_IP_ADDRESS: 400,
+        ERROR: 'Bad Request'
+    }
+
+
+def main():
+    try:  # загрузка параметров командной строки;
+        if '-p' in sys.argv:
+            listen_port = int(sys.argv[sys.argv.index('-p') + 1])
+        else:
+            listen_port = DEFAULT_PORT
+        if listen_port < 1024 or listen_port > 65535:
+            raise ValueError
+    except IndexError:
+        print('После параметра -\'p\' необходимо указать номер порта.')
+        sys.exit(1)
+    except ValueError:
+        print('В качастве порта может быть указано только число в диапазоне от 1024 до 65535.')
+        sys.exit(1)
+
+    try:  # загрузка адреса;
+        if '-a' in sys.argv:
+            listen_address = sys.argv[sys.argv.index('-a') + 1]
+        else:
+            listen_address = ''
+    except IndexError:
+        print('После параметра \'a\'- необходимо указать адрес, который будет слушать сервер.')
+        sys.exit(1)
+
+    transport = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    transport.bind((listen_address, listen_port))
+    transport.listen(MAX_CONNECTIONS)
+
+    while True:
+        client, client_address = transport.accept()
+        try:
+            message_from_client = get_message(client)
+            print(message_from_client)
+            response = process_client_message(message_from_client)
+            send_message(client, response)
+            client.close()
+        except (ValueError, json.JSONDecodeError):
+            print('Принято некорретное сообщение от клиента.')
+            client.close()
+
+
+if __name__ == '__main__':
+    main()
